10878a4d2d534fb661b7d7bb417cc8357d179e85
Marss616 <67536529+Marss616@users.noreply.github.com>
2024-08-12 23:26:13 +1000

    update

---
+++ Datetest.java
@@ -0,0 +1,13 @@
+
+import java.util.Date;
+
+public class Datetest {
+    public static void main(String[] args) {
+        int myAge = 30;
+        myAge = 35;
+        int herAge = myAge;
+        System.out.println(herAge);
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
